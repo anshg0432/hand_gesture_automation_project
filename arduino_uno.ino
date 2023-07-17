9c180a99be272a2952c3c137805d9dcc16ec1266
Ansh Gupta <89648595+anshg0432@users.noreply.github.com>
2023-07-17 19:35:59 +0530

    Update arduino_uno.ino

--- arduino_uno.ino
+++ arduino_uno.ino
@@ -16,7 +16,7 @@ void setup() {
   pinMode(b,OUTPUT);
   pinMode(c,OUTPUT);
   pinMode(acdPin,OUTPUT);
-  // acd.begin(NORMAL_MODE, OFF);
+ 
  
   Serial.begin(9600);
 
@@ -25,21 +25,15 @@ void setup() {
 void loop() {
   //put your main code here, to run repeatedly:
    if (Serial.available()){
-    // char command = Serial.read();
-    // if(command == 'A'){
-    // digitalWrite(a,HIGH);}
-    // else if(command=='B')
-    // digitalWrite(a,LOW);
+    
     String inputString = Serial.readStringUntil('\n');
-    // int number1 = inputString.substring(0, inputString.indexOf(',')).toInt();
-    // int number2 = inputString.substring(inputString.indexOf(',') + 1).toInt();
-    // int number3 = inputString.substring(inputString.indexOf(',') + 2).toInt();
+   
     int number1 = inputString.substring(0, inputString.indexOf(',')).toInt();
     inputString.remove(0, inputString.indexOf(',') + 1);
     int number2 = inputString.substring(0, inputString.indexOf(',')).toInt();
     inputString.remove(0, inputString.indexOf(',') + 1);
     int number3 = inputString.toInt();
-    //int num = Serial.parseInt();
+    
     if(number2==0){
       digitalWrite(a,HIGH);
       digitalWrite(c,LOW);
@@ -48,7 +42,7 @@ void loop() {
       digitalWrite(c,HIGH);
     }
     analogWrite(b,number1);
-    //int val = map(number1,0,255,30,70);
+    
     if(number3==1){
       digitalWrite(acdPin,HIGH);
     }
